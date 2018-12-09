172a8faba3314a6487876676a92666cbd1c5e9e3
maxerace <abhishekshankar.n1995@gmail.com>
2018-12-08 21:45:19 -0500

    Notification with map intent call

--- app/src/main/java/com/example/mobileapp/busybeeshopper/GetNearbyPlacesData.java
+++ app/src/main/java/com/example/mobileapp/busybeeshopper/GetNearbyPlacesData.java
@@ -3,6 +3,9 @@ package com.example.mobileapp.busybeeshopper;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
@@ -11,16 +14,20 @@ import android.database.sqlite.SQLiteDatabase;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.media.RingtoneManager;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.NotificationCompat;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.model.LatLng;
 
 
 import java.io.IOException;
@@ -33,7 +40,8 @@ import android.database.Cursor;
 public class GetNearbyPlacesData extends Service {
     private int PROXIMITY_RADIUS = 50;
     LocationManager locationManager;
-    LocationListener locationListener;
+    LocationListener locationListener1;
+    LocationListener locationListener2;
     double latitude;
     double longitude;
     String googlePlacesData;
@@ -48,7 +56,7 @@ public class GetNearbyPlacesData extends Service {
         super.onCreate();
         Log.d(TAG, "onCreate: service calle");
         locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-        locationListener = new LocationListener() {
+        locationListener1 = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
                 latitude = location.getLatitude();
@@ -82,7 +90,70 @@ public class GetNearbyPlacesData extends Service {
         };
 
 
-        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 50, locationListener);
+
+        locationListener2 = new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                latitude = location.getLatitude();
+                longitude = location.getLongitude();
+                SQLiteDatabase database = openOrCreateDatabase("mydata",MODE_PRIVATE,null);
+                database.execSQL("CREATE TABLE IF NOT EXISTS Entries(Latitude DOUBLE,Longitude DOUBLE,Name VARCHAR,Vicinity VARCHAR)");
+                Cursor c = database.rawQuery("Select * from Entries", null);
+                int Latitude = c.getColumnIndex("Latitude");
+                int Longitude = c.getColumnIndex("Longitude");
+                int name = c.getColumnIndex("Name");
+                int Vicinity = c.getColumnIndex("Vicinity");
+                while (c.moveToNext()) {
+                    LatLng latLng = new LatLng(c.getDouble(Latitude), c.getDouble(Longitude));
+                    Location location2 = new Location("");
+                    location2.setLongitude(c.getDouble(Longitude));
+                    location2.setLatitude(c.getDouble(Latitude));
+                    String placeName = c.getString(name);
+                    String vicinity = c.getString(Vicinity);
+                    float distance = location2.distanceTo(location);
+                    if (distance < 20){
+                        // Build notification
+                        // Actions are just fake
+                        Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                        PendingIntent pIntent = PendingIntent.getActivity(getApplicationContext(), (int) System.currentTimeMillis(), intent, 0);
+                        Notification noti = new Notification.Builder(getApplicationContext())
+                                .setContentTitle("Onr of the items you wish to purchase is available at "+placeName)
+                                .setContentText("Subject").setSmallIcon(R.drawable.bussybee)
+                                .setContentIntent(pIntent).build();
+                                //.addAction(R.drawable.icon, "Call")
+                                //.addAction(R.drawable.icon, "More", pIntent)
+                                //.addAction(R.drawable.icon, "And more", pIntent).build();
+                        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+                        noti.defaults |= Notification.DEFAULT_SOUND;
+
+                        noti.flags |= Notification.FLAG_AUTO_CANCEL;
+
+                        notificationManager.notify(0, noti);
+                    }
+
+                }
+
+
+            }
+
+            @Override
+            public void onStatusChanged(String s, int i, Bundle bundle) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String s) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String s) {
+
+            }
+        };
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 10000, locationListener1);
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 20, locationListener2);
+
     }
 
 
