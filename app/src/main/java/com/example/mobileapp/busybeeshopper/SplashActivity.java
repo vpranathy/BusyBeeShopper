1c44169f19a7f012482108ea0b321dfa348f0161
vpranathy <vpranathy@gmail.com>
2018-12-10 18:17:22 -0500

    Added opening animation

--- app/src/main/java/com/example/mobileapp/busybeeshopper/SplashActivity.java
+++ app/src/main/java/com/example/mobileapp/busybeeshopper/SplashActivity.java
@@ -1,17 +1,78 @@
 package com.example.mobileapp.busybeeshopper;
 
 import android.content.Intent;
+import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
 
-        Intent intent = new Intent(this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        /**
+         * The thread to process splash screen events
+         */
+        private Thread mSplashThread;
+
+        /** Called when the activity is first created. */
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+
+            // Splash screen view
+            setContentView(R.layout.splash_activity);
+
+            final SplashActivity sPlashScreen = this;
+            final ImageView splashImageView = (ImageView) findViewById(R.id.iv_icons);
+
+            splashImageView.setBackgroundResource(R.drawable.splash_animation);
+            final AnimationDrawable frameAnimation = (AnimationDrawable)splashImageView.getBackground();
+            splashImageView.post(new Runnable(){
+                @Override
+                public void run() {
+                    frameAnimation.start();
+                }
+            });
+
+            // The thread to wait for splash screen events
+            mSplashThread =  new Thread(){
+                @Override
+                public void run(){
+                    try {
+                        synchronized(this){
+                            // Wait given period of time or exit on touch
+                            wait(1610);
+                        }
+                    }
+                    catch(InterruptedException ex){
+                    }
+
+                    finish();
+
+                    // Run next activity
+                    Intent intent = new Intent();
+                    intent.setClass(sPlashScreen, LoginActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            };
+
+            mSplashThread.start();
+        }
+
+        /**
+         * Processes splash screen touch events
+         */
+        @Override
+        public boolean onTouchEvent(MotionEvent evt)
+        {
+            if(evt.getAction() == MotionEvent.ACTION_DOWN)
+            {
+                synchronized(mSplashThread){
+                    mSplashThread.notifyAll();
+                }
+            }
+            return true;
+        }
     }
-}
